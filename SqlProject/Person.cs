c1cc7869a2bf29a76b9ccf9f14b0ce7352ba64fa
GeorgeSmileyface <104126452+GeorgeSmileyface@users.noreply.github.com>
2025-04-07 10:52:37 +0200

    Add project files.

---
+++ SqlProject/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlProject
+{
+    public class Person
+    {
+        //create a person that has an int as id, a first name, a last name, an adress and a city as strings
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+
+        public Person(int id, string firstName, string lastName, string adress, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Adress = adress;
+            City = city;
+        }
+    }
+}
---
+++ SqlProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using SqlProject;
+using System.Data.SqlClient;
+
+string connectionString = "Server = localhost;" +
+    "Database = SqlTeachingDb;" +
+    "User Id = sa;" +
+    "Password = 1234;" +
+    "Trusted_Connection = True;";
+
+var person = new Person(1, "John", "Doe", "1234 Elm St", "Springfield); DROP TABLE Persons;");
+
+SqlConnection connection = new SqlConnection(connectionString);
+
+SqlCommand command = connection.CreateCommand();
+string sql = "INSERT INTO Persons (PersonID, FirstName, LastName, Address, City)" +
+    " VALUES (@PersonID, @FirstName, @LastName, @Address, @City)";
+
+command.CommandText = sql;
+command.Parameters.AddWithValue("@PersonID", person.Id);
+command.Parameters.AddWithValue("@FirstName", person.FirstName);
+command.Parameters.AddWithValue("@LastName", person.LastName);
+command.Parameters.AddWithValue("@Address", person.Adress);
+command.Parameters.AddWithValue("@City", person.City);
+
+connection.Open();
+
+command.ExecuteNonQuery();
+
+connection.Close();
+
+Console.ReadKey();
